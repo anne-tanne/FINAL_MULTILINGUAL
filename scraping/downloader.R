300c388368dfd1e2edd82dca339d57ac3a2fbe15
Anne-Sophie Skarabis <61362884+anne-tanne@users.noreply.github.com>
2023-11-11 13:50:17 +0100

    Update downloader.R
    Fixed issue where only one language edition was scraped.

--- scraping/downloader.R
+++ scraping/downloader.R
@@ -1,54 +1,63 @@
 library(tidyverse)
 library(httr)
 
-# Define the function to download HTML files
-download_html <- function(df, target_dir) {
-  df |>
-    mutate(
-      # Convert date to character and replace special characters in the title
-      date = as.character(date),
-      title = gsub(" ", "-", title),
-      title = iconv(title, from = "UTF-8", to = "ASCII//TRANSLIT") # transliterate ä, ö, ü to ae, oe, ue
-    ) |>
-    rowwise() |>
-    do({
-      try({
-        # Construct the filename
-        filename <- paste0(.$date, "_", .$title, ".html")
-        
-        # Create the complete path for the file
-        file_path <- file.path(target_dir, filename)
-        
-        # Download the file
-        GET(.$loc, write_disk(file_path, overwrite = TRUE))
-        Sys.sleep(2) # Sleep for 2 seconds to throttle the download speed
-      }, silent = TRUE) # If an error occurs (like a 404), it will skip to the next file
-    })
+# Function to transliterate special characters and download HTML files
+download_html_files <- function(csv_file_path, target_dir) {
+  # Read the CSV file
+  df <- read_csv(csv_file_path, show_col_types = FALSE)
+  
+  # Iterate over each row in the dataframe
+  for(i in 1:nrow(df)) {
+    # Create filename from date and title
+    date <- gsub("-", "", df$date[i])  # Remove hyphens from date
+    title <- tolower(df$title[i])  # Convert title to lower case
+    title <- gsub(" ", "-", title)  # Replace spaces with hyphens
+    title <- iconv(title, to = "ASCII//TRANSLIT")  # Transliterate characters like ä, ö, ü to ae, oe, ue
+    filename <- paste0(date, "_", title, ".html")
+    
+    # Specify the path for the file
+    file_path <- file.path(target_dir, filename)
+    
+    # Print message to console
+    message("Downloading: ", filename)
+    
+    # Download the file and handle errors silently
+    try({
+      GET(df$loc[i], write_disk(file_path, overwrite = TRUE))
+      message("Downloaded: ", filename)
+      Sys.sleep(2)  # Sleep for 2 seconds between downloads
+    }, silent = TRUE)
+  }
 }
 
-# Create a new 'HTML' directory and subdirectories for each publication
-base_dir <- getwd() # Replace this with the path to the base directory if not running from the base directory
+# Define base directory and subdirectories
+base_dir <- getwd()
+csv_final_dir <- file.path(base_dir, "csv_files", "final")
 html_dir <- file.path(base_dir, "HTML")
+
+# Create the 'html' directory and subdirectories for each publication if they don't exist
 dir.create(html_dir, showWarnings = FALSE, recursive = TRUE)
 dir.create(file.path(html_dir, "20minuti"), showWarnings = FALSE)
 dir.create(file.path(html_dir, "20minuten"), showWarnings = FALSE)
 dir.create(file.path(html_dir, "20minutes"), showWarnings = FALSE)
 
-# Define the path to the 'final' directory where the CSVs are located
-csv_final_dir <- file.path(base_dir, "csv_files", "final")
+# List of publication names
+publications <- c("20minuti", "20minuten", "20minutes")
 
-# Get the list of CSV files
-csv_files <- list.files(csv_final_dir, pattern = "processed_.*\\.csv$", full.names = TRUE)
-
-# Download the HTML files for each CSV file
-for (csv_file in csv_files) {
-  file_info <- str_match(basename(csv_file), "processed_(.*?)\\.csv")
-  publication_name <- file_info[2]
-  target_dir <- file.path(html_dir, publication_name)
+# Process each publication's CSV files
+for (publication in publications) {
+  # Define the path to the current publication's CSV file
+  csv_file_path <- file.path(csv_final_dir, paste0("processed_", publication, ".csv"))
   
-  # Read the CSV file
-  df <- read_csv(csv_file, show_col_types = FALSE)
+  # Define the target directory for the HTML files
+  target_dir <- file.path(html_dir, publication)
   
-  # Download the HTML content using the function
-  download_html(df, target_dir)
+  # Print message to console
+  message("Starting downloads for ", publication)
+  
+  # Download the HTML files for the current publication
+  download_html_files(csv_file_path, target_dir)
+  
+  # Print message to console
+  message("Completed downloads for ", publication)
 }
