4efb09430c7f9ae0a03b2f296bdac73883482edf
Anne-Sophie Skarabis <61362884+anne-tanne@users.noreply.github.com>
2023-12-02 13:59:14 +0100

    Update fr_extractor.R

--- scraping/fr_extractor.R
+++ scraping/fr_extractor.R
@@ -19,24 +19,24 @@ process_file <- function(file_path) {
     html_content <- read_html(file_path)
     
     # Extract the genre from the <span> within the <h2> tag and remove the colon
-    genre <- html_content %>%
-      html_nodes(xpath = '//h2[not(@class)]/span[not(@class)]') %>%
-      html_text(trim = TRUE) %>%
-      str_squish() %>%
+    genre <- html_content|>
+      html_nodes(xpath = '//h2[not(@class)]/span[not(@class)]')|>
+      html_text(trim = TRUE)|>
+      str_squish()|>
       str_remove(":") # Remove the colon
     genre <- ifelse(length(genre) > 0, genre, NA)
     
     # Extract the title from the <h2> tag, excluding any nested <span> tags
-    title <- html_content %>%
-      html_nodes(xpath = '//h2[not(@class)]/text()') %>%
-      html_text(trim = TRUE) %>%
+    title <- html_content|>
+      html_nodes(xpath = '//h2[not(@class)]/text()')|>
+      html_text(trim = TRUE)|>
       str_squish()
     title <- ifelse(length(title) > 0, title, NA)
     
     # Extract the main content from the <p> tags within the class 'Article_elementTextblockarray__1gb_B'
-    main_content <- html_content %>%
-      html_nodes('.Article_elementTextblockarray__1gb_B p') %>%
-      html_text(trim = TRUE) %>%
+    main_content <- html_content|>
+      html_nodes('.Article_elementTextblockarray__1gb_B p')|>
+      html_text(trim = TRUE)|>
       str_squish()
     main_content_combined <- ifelse(length(main_content) > 0, paste(main_content, collapse=" "), NA)
     
